e1b6125aea3746d410c11768bf1c626e429defad
HARIVOLA Andy <hei.andy.8@gmail.com>
2023-06-15 11:14:41 +0300

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 /**
   Calculates the time difference between the server time and client time.
@@ -9,12 +11,47 @@ import { useRouter } from "next/router";
 */
 const calculateTimeDifference = (server: Date, client: Date) => {};
 
+interface TimeProps {
+  serverData: number;
+}
 
-export default function Home() {
+export const getServerSideProps: GetServerSideProps<TimeProps> = async () => {
+  const serverTimesDate = new Date();
+  const serverTimes = serverTimesDate.getTime()
+  return {
+    props: {
+      serverData: serverTimes,
+    },
+  };
+};
+
+
+const Home: React.FC<TimeProps> = ({serverData}) => {
+  const [timeDifference, setTimeDifference] = useState<number|null>(null);
   const router = useRouter();
+
+  useEffect(()=>{
+    const currentTime = new Date();
+    const currentTimeNumber = currentTime.getTime()
+    if (typeof serverData == "number") {
+      setTimeDifference(Math.abs(serverData-currentTimeNumber))
+    }
+  },[serverData])
+
+  const formatTimeDifference = (difference:number) => {
+    // Convertir la différence en jours, heures, minutes et secondes
+    const seconds = Math.floor(difference / 1000) % 60;
+    const minutes = Math.floor(difference / (1000 * 60)) % 60;
+    const hours = Math.floor(difference / (1000 * 60 * 60)) % 24;
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+    return ` ${days} jours, ${hours} heures, ${minutes} minutes, ${seconds} secondes`;
+  };
+
   const moveToTaskManager = () => {
     router.push("/tasks");
   }
+
   return (
     <>
       <Head>
@@ -29,13 +66,13 @@ export default function Home() {
           {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
             Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{" " + serverData!=null ? formatTimeDifference(serverData):" " }</span>
           </p>
 
           {/* Display here the time difference between the server side and the client side */}
           <p>
             Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{" " +  timeDifference == null ?" it's null ": formatTimeDifference(timeDifference??0)}</span>
           </p>
         </div>
 
@@ -46,3 +83,5 @@ export default function Home() {
     </>
   );
 }
+
+export default Home;
