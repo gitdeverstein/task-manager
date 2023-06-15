711dcc5f3ac33833f8b6b2cf28d693dae09219f1
HARIVOLA Andy <hei.andy.8@gmail.com>
2023-06-15 11:58:25 +0300

    chore: add local storage in useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,8 @@
 const useLocalStorage = () => {
-
+  return {
+    read: (key: string) => localStorage.getItem(key),
+    save: (key: string, value: string) => localStorage.setItem(key, value),
+  };
 }
 
 export {
